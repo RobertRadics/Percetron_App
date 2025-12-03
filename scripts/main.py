7b599b54b099bc82fe73591b889079df213971ed
RobertRadics <radicspal968@gmail.com>
2025-12-03 16:58:09 +0100

    További importok behejézse másik fájlokból

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,10 @@
 import os
 import sys
 from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+from src.perceptron import Perceptron
+from src.data_loader import DataLoader
+from src.visualizer import Visualizer
+import numpy as np
