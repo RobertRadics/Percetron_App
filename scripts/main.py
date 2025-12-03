67ae798765f9dc452b42f9d3f472a8da869f5ff0
RobertRadics <radicspal968@gmail.com>
2025-12-03 16:59:01 +0100

    konzolos menüpontok létrehozva

--- scripts/main.py
+++ scripts/main.py
@@ -8,3 +8,19 @@ from src.perceptron import Perceptron
 from src.data_loader import DataLoader
 from src.visualizer import Visualizer
 import numpy as np
+
+def print_menu():
+    print("\n" + "="*60)
+    print("INTERAKTÍV PERCEPTRON TANÍTÓ ALKALMAZÁS")
+    print("="*60)
+    print("1. Adatfájl betöltése")
+    print("2. Adatok megjelenítése")
+    print("3. Perceptron inicializálása")
+    print("4. Egy lépés tanítás")
+    print("5. Teljes tanítás futtatása")
+    print("5a. Interaktív tanítás (epochonkénti megállás)")
+    print("6. Döntési határ megjelenítése")
+    print("7. Konvergencia görbe megjelenítése")
+    print("8. Paraméterek beállítása")
+    print("9. Kilépés")
+    print("="*60)
