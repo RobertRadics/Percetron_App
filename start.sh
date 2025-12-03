ac88b95c57188a93e1d709b05bd4449d37616ce4
RobertRadics <radicspal968@gmail.com>
2025-12-03 17:20:19 +0100

    Sh fájl szerkesztésének befejezése

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "========================================"
+echo "Perceptron Tanito Alkalmazas inditasa"
+echo "========================================"
+echo ""
+echo "A bongeszo automatikusan megnyilik..."
+echo ""
+streamlit run scripts/app.py
+
+
