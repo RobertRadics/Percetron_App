9e0b8c0705f604da2af677c1c8e053f21fc5fb64
RobertRadics <radicspal968@gmail.com>
2025-12-03 17:15:44 +0100

    Bat fájl szerkesztése befejezve

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ========================================
+echo Perceptron Tanito Alkalmazas inditasa
+echo ========================================
+echo.
+echo A bongeszo automatikusan megnyilik...
+echo.
+python -m streamlit run scripts/app.py
+pause
+
